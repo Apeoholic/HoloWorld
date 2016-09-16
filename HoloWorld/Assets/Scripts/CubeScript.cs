47c31c79c781a7b1b86bece0353fbf74c089a68d
Jimmy Engström <jimmy@apeoholic.se>
2016-09-16 15:53:56 +0200

    Added Gaze

---
+++ HoloWorld/Assets/Scripts/CubeScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Events;
+
+public class CubeScript:MonoBehaviour
+{
+    public void OnGazeEnter()
+    {
+        gameObject.GetComponent<MeshRenderer>().material.color = Color.red;
+    }
+
+
+    public void OnGazeLeave()
+    {
+        gameObject.GetComponent<MeshRenderer>().material.color = Color.white;
+    }
+}
