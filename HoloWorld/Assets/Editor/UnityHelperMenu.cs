a8a561878a9426b1fe2479ee2a4e0c15e2d5afa8
Jimmy Engström <jimmy@apeoholic.se>
2016-09-16 13:46:15 +0200

    Added folders and editor script

---
+++ HoloWorld/Assets/Editor/UnityHelperMenu.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public static class UnityHelperMenu
+{
+    [MenuItem("UnityHelpers/Create folder structure", priority = 1)]
+    public static void CreateFolderStructure()
+    {
+        var folders = new string[] {"Scripts","Prefabs","Meshes","Textures","Materials","Sounds","Resources","Scenes" };
+        foreach (var f in folders)
+        {
+            AssetDatabase.CreateFolder("Assets", f);
+        }
+    }
+}
